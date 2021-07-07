27c05000c1d26f085f5e507583d8207835ccd412
Your Name <you@example.com>
2021-07-07 16:03:17 +0800

    add-props Add autoHeight option to allow the content to determine the height of the border box

--- src/components/FancyOrnamental/FancyOrnamental.jsx
+++ src/components/FancyOrnamental/FancyOrnamental.jsx
@@ -36,7 +36,6 @@ const FancyOrnamental = (props) => {
   `
 
   const InnerContainer = styled.div`
-    position: absolute;
     width: 100%;
     height: 100%;
     left: 0;
--- src/components/FancyOrnamental/FancyOrnamental.stories.js
+++ src/components/FancyOrnamental/FancyOrnamental.stories.js
@@ -21,7 +21,25 @@ export const Default = () => {
         gapSize={140} 
         borderSize={1} 
         roundedSize={3}
-      >Cras est metus, iaculis a dictum eget, suscipit vitae quam. Mauris sollicitudin lectus nec mollis tristique. Fusce sagittis, nulla et gravida elementum, orci nisl pellentesque lacus, eget tempor nibh erat nec elit. Aenean ante massa, varius vitae ullamcorper vel, iaculis a urna. Vestibulum ultricies semper nisl quis malesuada. Fusce ullamcorper nunc non tempor consectetur. Donec at turpis ac dui interdum maximus in eget dui. Vestibulum viverra velit quis efficitur dictum. Ut molestie nibh vel lectus sollicitudin bibendum. Aenean suscipit eget purus sed fermentum.</FancyOrnamental>
+      ></FancyOrnamental>
+    </div>
+  );
+
+}
+
+export const AutoHeight = () => {
+  
+  return (
+    <div style={{height: '100vh', width: '100%', boxSizing: 'border-box', padding: '100px'}}>
+      <FancyOrnamental 
+        topOrnament={<Ornament/>} 
+        ornamentHeight={48} 
+        borderSize={1} 
+        roundedSize={3}
+        autoHeight={true}
+      ><div style={{boxSizing:'border-box', padding: '60px'}}>
+          Cras est metus, iaculis a dictum eget, suscipit vitae quam. Mauris sollicitudin lectus nec mollis tristique. Fusce sagittis, nulla et gravida elementum, orci nisl pellentesque lacus, eget tempor nibh erat nec elit. Aenean ante massa, varius vitae ullamcorper vel, iaculis a urna. Vestibulum ultricies semper nisl quis malesuada. Fusce ullamcorper nunc non tempor consectetur. Donec at turpis ac dui interdum maximus in eget dui. Vestibulum viverra velit quis efficitur dictum. Ut molestie nibh vel lectus sollicitudin bibendum. Aenean suscipit eget purus sed fermentum.
+        </div></FancyOrnamental>
     </div>
   );
 
